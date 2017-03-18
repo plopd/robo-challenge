36a31da5acea6219644593264e02a4db10aa8309
plopd <daniel.plop@tum.de>
2017-03-18 01:30:30 +0100

    Remove sumo challenge from rover branch

--- challenges/sumo/framework/groovy/robot_groovy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/home/robot/ejre1.7.0_60
-export GROOVY_HOME=/home/robot/groovy-2.4.7
-
-export PATH=$PATH:$JAVA_HOME/bin:$GROOVY_HOME/bin
-
-groovy -Dgroovy.grape.report.downloads=true -Divy.message.logger.level=4 -Divy.cache.ttl.default=24h Robot.groovy
